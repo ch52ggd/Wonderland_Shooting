d3ffae7db04511034b4df0e3f5520a96a6d2b4d7
ch52ggd <20200887@gm.hannam.ac.kr>
2023-10-10 17:12:10 +0900

    프로젝트 생성
    프로젝트 생성

---
+++ cache/js/_editor_index.js
@@ -0,0 +1,4 @@
+import * as __wonderlandengine_components from '@wonderlandengine/components';
+_registerEditor(__wonderlandengine_components);
+import * as _E__git_CHOIJiho_Wonderland_Shooting_js_button_js from 'E:/git_CHOIJiho/Wonderland_Shooting/js/button.js';
+_registerEditor(_E__git_CHOIJiho_Wonderland_Shooting_js_button_js);
